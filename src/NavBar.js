cb4243a7cdf1ed8d2931816b75a82bc2b72f66e1
Adam Barton <adamb6102@gmail.com>
2020-01-30 16:32:42 -0800

    When screen size is too small, the navbar items get turned into a button. Need to add the functionality.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './NavBar.css';
 import { NavLink } from 'react-router-dom';
 
 import dataTypes from './data/dataTypes.json'
 
 
+function NavBarContent() {
+  let width = window.innerWidth;
+
+  if (width > 1300) {
+    return (
+      <div className='navbar'>
+        <NavLink className="title" to="/">Northwest Vision</NavLink>
+        <ul>
+          {dataTypes["dataTypes"].map(({name, url}) => {
+            return (<li className="navlink" key={url}>
+              <NavLink to={url}>
+                {name}
+              </NavLink>
+            </li>);
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className='navbar'>
+        <NavLink className="title" to="/">Northwest Vision</NavLink>
+        <button>
+          ASDF
+        </button>
+      </div>
+    );
+  }
+}
+
 function NavBar(props) {
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return (() => {
+      window.removeEventListener('resize', handleResize)});
+  });
+
   return (
-    <div className='navbar'>
-      <NavLink className="title" to="/">Northwest Vision</NavLink>
-      <ul>
-        {dataTypes["dataTypes"].map(({name, url}) => {
-          return (<li className="navlink" key={url}>
-            <NavLink to={url}>
-              {name}
-            </NavLink>
-          </li>);
-        })}
-      </ul>
+    <div>
+      {NavBarContent()}
     </div>
   );
 }
