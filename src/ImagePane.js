72d3a28845918cdecdde185a4470d6b3a8463dc0
Adam Barton <adamb6102@gmail.com>
2020-01-31 20:22:45 -0800

    Fixed some css and added in some more images in preparation for lazy-load implementation.

--- src/ImagePane.js
+++ src/ImagePane.js
@@ -5,9 +5,11 @@ import { NavLink } from 'react-router-dom';
 
 function ImagePane(props) {
   return (
-    <NavLink to={props.source}>
-      <img className={props.alignment} src={props.source} alt={props.alt} />
-    </NavLink>
+    <div className="pane">
+      <NavLink to={props.source}>
+        <img className={props.alignment} src={props.source} alt={props.alt} />
+      </NavLink>
+    </div>
   );
 }
 
