2dec27bd077237c0cbe0bffffc61362b915331db
Adam Barton <adamb6102@gmail.com>
2020-03-01 22:03:02 -0800

    Got carousel animations for sliding working.

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -33,6 +33,11 @@ function Content(props) {
     width: 100%;
     z-index: 1;
 
+    ${'' /* animation: ${props.slideRightTrigger ? "slide" : ""};
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+    animation-direction: normal;
+    animation-fill-mode: forwards; */}
     transition: background-color 0.5s cubic-bezier(.4,.38,.12,1);
     background-color: ${props.item.bgColor};
 
@@ -43,23 +48,103 @@ function Content(props) {
 
     overflow: hidden;
 
-    img {
+    .disp-img {
       ${'' /* border: 2px solid blue; */}
+
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
 
       width: 100%;
       ${'' /* max-height: 100%; */}
+    }
 
+    .prev-img {
+      ${'' /* border: 2px solid red; */}
+
+      position: absolute;
+      top: 0;
+      left: -100%;
+      height: 100%;
+      width: 100%;
+      z-index: 1;
+
+
+      animation: ${props.slideLeftTrigger ? "slide-backwards" : ""};
+      animation-duration: 0.8s;
+      animation-iteration-count: 1;
+      animation-direction: normal;
+      animation-fill-mode: forwards;
+      transition: background-color 0.5s cubic-bezier(.4,.38,.12,1)
+                  animation 1s ease;
+    }
+
+    .next-img {
+      ${'' /* border: 2px solid green; */}
+
+      position: absolute;
+      top: 0;
+      left: 100%;
+      height: 100%;
+      width: 100%;
+
+
+      animation: ${props.slideRightTrigger ? "slide-forwards" : ""};
+      animation-duration: 0.8s;
+      ${'' /* animation-iteration-count: 1; */}
+      animation-direction: normal;
+      animation-fill-mode: forwards;
+      transition: background-color 0.5s cubic-bezier(.4,.38,.12,1)
+                  animation 1s ease;
+    }
+
+
+    @keyframes slide-forwards {
+      from {
+        left: 100%;
+      }
+      to {
+        left: 0;
+      }
+    }
+
+    @keyframes slide-backwards {
+      from {
+        left: -100%;
+      }
+      to {
+        left: 0;
+      }
     }
   `;
   return (
     <div css={styling}>
-      <img src={ImageImp(props.item.source)}
+      {props.prevItem != null ?
+        <img className="prev-img"
+          src={ImageImp(props.prevItem.source)}
+          alt="" />
+        :
+        <div></div>
+      }
+      <img className="disp-img"
+        src={ImageImp(props.item.source)}
         alt="" />
+      {props.nextItem != null ?
+        <img className="next-img"
+          src={ImageImp(props.nextItem.source)}
+          alt="" />
+        :
+        <div></div>
+      }
   	</div>
   );
 }
 
 function Carousel() {
+  const [ slideRightTrigger, setSlideRightTrigger ] = useState(false);
+  const [ slideLeftTrigger, setSlideLeftTrigger ] = useState(false);
   const [ current, setCurrent ] = useState(0);
 
   const styling = css`
@@ -72,18 +157,47 @@ function Carousel() {
     width: 100%;
     z-index: -1;
   `;
+
+
+    async function updateSlide(newCurrent) {
+      let responseBody = {};
+      console.log("BEFORE");
+      if (newCurrent > current) {
+        console.log("RIGHT");
+        setSlideRightTrigger(true)
+      } else {
+        console.log("LEFT");
+        setSlideLeftTrigger(true)
+      }
+      await new Promise(r => setTimeout(r, 800));
+      console.log("AFTER");
+      setCurrent(newCurrent)
+      setSlideRightTrigger(false)
+      setSlideLeftTrigger(false)
+    }
+
+
   return (
     <div css={styling}>
       {current === 0 ?
-        <ArrowRight current={current} setCurrent={setCurrent} accentColor={pageData[current].accentColor} /> :
+        <div>
+          <ArrowRight current={current} setCurrent={updateSlide} accentColor={pageData[current].accentColor} />
+          <Content slideRightTrigger={slideRightTrigger} slideLeftTrigger={slideLeftTrigger} item={pageData[current]} nextItem={pageData[current+1]} prevItem={null} />
+        </div> :
         current === pageData.length-1 ?
-          <ArrowLeft current={current} setCurrent={setCurrent} accentColor={pageData[current].accentColor} /> :
           <div>
-            <ArrowLeft current={current} setCurrent={setCurrent} accentColor={pageData[current].accentColor} />
-            <ArrowRight current={current} setCurrent={setCurrent} accentColor={pageData[current].accentColor} />
+            <ArrowLeft current={current} setCurrent={updateSlide} accentColor={pageData[current].accentColor} />
+            <Content slideRightTrigger={slideRightTrigger} slideLeftTrigger={slideLeftTrigger} item={pageData[current]} prevItem={pageData[current-1]} nextItem={null} />
+          </div> :
+          <div>
+            <ArrowLeft current={current} setCurrent={updateSlide} accentColor={pageData[current].accentColor} />
+            <ArrowRight current={current} setCurrent={updateSlide} accentColor={pageData[current].accentColor} />
+            {console.log("CURRENT " + current)}
+            <Content slideRightTrigger={slideRightTrigger} slideLeftTrigger={slideLeftTrigger} item={pageData[current]} prevItem={pageData[current-1]} nextItem={pageData[current+1]} />
+            {/* <Content item={pageData[current]} /> */}
           </div>}
 
-      <Content item={pageData[current]} />
+      {/* <Content slideRightTrigger={slideRightTrigger} item={pageData[current]} nextItem={pageData[current+1]} /> */}
       <ProgressDots current={current} data={pageData} setCurrent={setCurrent} accentColor={pageData[current].accentColor} />
   	</div>
   );
