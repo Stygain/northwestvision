7d53addd2557a7ca6d0035ae38aefed79a2453b4
Adam Barton <adamb6102@gmail.com>
2021-11-06 15:17:30 -0700

    Attempts to add google analytics to the site.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import GA from './GoogleAnalytics.js';
 
 ReactDOM.render(
   <HashRouter>
+    { GA.init() && <GA.RouteTracker /> }
     <App />
   </HashRouter>,
   document.getElementById('root'));
